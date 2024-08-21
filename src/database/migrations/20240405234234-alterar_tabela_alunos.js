119be4d6503f8e38320100602b23b601633cd37f
Daiane Arruda <daianedasilvaarruda@outlook.com>
2024-08-21 16:43:53 -0300

    finalizando role

--- src/database/migrations/20240405234234-alterar_tabela_alunos.js
+++ src/database/migrations/20240405234234-alterar_tabela_alunos.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn('alunos', {
+    await queryInterface.addColumn('alunos', 'celular', {
       type: Sequelize.STRING,
       allowNull: true
     });
@@ -11,7 +11,7 @@ module.exports = {
 
   async down (queryInterface, Sequelize) {
 
-    await queryInterface.removeColumn('alunos');
+    await queryInterface.removeColumn('alunos', 'celular');
 
   }
 };
--- src/database/migrations/20240419004416-alter_table_adicionar_login_alunos.js
+++ src/database/migrations/20240419004416-alter_table_adicionar_login_alunos.js
@@ -3,15 +3,15 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn('alunos', 'email', {
-      type: Sequelize.STRING,
-      allowNull: false,
-    });
+    // await queryInterface.addColumn('alunos', 'email', {
+    //   type: Sequelize.STRING,
+    //   allowNull: false,
+    // });
 
-    await queryInterface.addColumn('alunos', 'password', {
-      type: Sequelize.STRING,
-      allowNull: false,
-    });
+    // await queryInterface.addColumn('alunos', 'password', {
+    //   type: Sequelize.STRING,
+    //   allowNull: false,
+    // });
   },
 
   async down (queryInterface, Sequelize) {
